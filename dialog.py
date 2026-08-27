44420371472d2b6ec7aa779fbf40f70fac1e1cd7
valsev99 <valhag03@edu.umea.se>
2026-08-27 13:32:20 +0200

    lekton torsdag

---
+++ dialog.py
@@ -0,0 +1,4 @@
+name = input ("vad heter du?" )
+print("hej " + name + "!")
+ålder = int(input("Hur gammal är du? "))
+print(ålder + 5)
---
+++ variabler.py
@@ -0,0 +1,9 @@
+namn = "Gilbert"
+ålder = 88
+är_vinnare = True
+tiden = 16.8
+
+print(namn)
+print(ålder)
+print(är_vinnare)
+print(tiden)
